8441e35eac21308a6ea2dcfd9944ec6271ec9dfc
lukmanbdg <32958326+lukmanbdg@users.noreply.github.com>
2017-10-21 09:48:33 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,9 @@
+package latihan1;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+   
+}
