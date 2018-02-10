aa3a3e33438f88ea92bd72382a0bd65edce9665b
lukmanbdg <32958326+lukmanbdg@users.noreply.github.com>
2018-02-10 10:02:15 +0700

    Delete Latihan1.java

--- Latihan1.java
+++
@@ -1,9 +0,0 @@
-package latihan1;
-
-public class Latihan1 {
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World");
-    }
-   
-}
